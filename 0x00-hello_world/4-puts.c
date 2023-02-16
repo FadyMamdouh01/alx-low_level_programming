22bbccd2c7534e012e0543a84e87967d1c904f3a
FadyMamdouh01 <fadymamdouh37@gmail.com>
2023-02-16 15:01:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A Programming is like building a multilingual puzzle
+ n
+ * Return: 0 (Success)
+ */
+
+int main(viod)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
